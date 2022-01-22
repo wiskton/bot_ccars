1f68cbe526b07a0d19fb55a75a9d4b9904b4c338
Willem Allan <willemarf@outlook.com>
2022-01-22 07:54:08 -0300

    Add files via upload

---
+++ ccar.py
@@ -0,0 +1,137 @@
+import pyautogui
+import time
+import _thread
+import mouse
+import sys
+from pyautogui import press
+from PyQt5.QtWidgets import *
+
+# --- configure variables ---
+cars = [{
+    "races": 6,
+    "x": 421,
+    "y": 726
+}]
+
+menu_cars_x = 578
+menu_cars_y = 98
+close_race_x = 300
+close_race_y = 300
+
+# ---------------------------
+
+class Window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        self._running_bot = False
+        self._running__x = False
+
+        cbutton = QCheckBox("Bot ativo")
+        cbutton.setChecked(False)
+        cbutton.toggled.connect(self.onClickedBot)
+        layout.addWidget(cbutton, 0, 0)
+
+        cbutton_position = QCheckBox("Exibir X, Y")
+        cbutton_position.setChecked(False)
+        cbutton_position.toggled.connect(self.onClickedPosition)
+        layout.addWidget(cbutton_position, 10, 0)
+
+        self.thread = None 
+
+    def click(self, x, y):
+        mouse.move(x, y, absolute=True, duration=0.1)
+        mouse.click('left')
+        self.sleep(2)
+
+    def scroll(self, value):
+        pyautogui.vscroll(value)
+
+    def sleep(self, seconds):
+        time.sleep(5)
+
+    def rotine(self):
+        for car in cars:
+            print("5 segundos página será atualizada")
+            self.sleep(5)
+            if not self._running_bot:
+                return 
+
+            press('f5')
+
+            self.sleep(5)
+            if not self._running_bot:
+                return 
+
+            # menu cars
+            print("entrando no menu dos carros")
+            self.click(menu_cars_x, menu_cars_y)
+            if not self._running_bot:
+                return 
+
+            # scroll final
+            print("rolando página até o final")
+            self.scroll(-3000)
+            self.sleep(5)
+            if not self._running_bot:
+                return 
+
+            for race in range(1, car["races"]+1):
+                print("iniciando a corrida", race)
+                # start corrida
+                print(car["x"], car["y"])
+                self.click(car["x"], car["y"])
+                if not self._running_bot:
+                    return 
+
+                # aguarda corrida
+                print("aguardando a corrida terminar")
+                self.sleep(30)
+                if not self._running_bot:
+                    return 
+
+                # fecha janela
+                self.click(close_race_x, close_race_y)
+                if not self._running_bot:
+                    return 
+
+    def get_position(self):
+        while self._running_position:
+            x, y = pyautogui.position()
+            print("x={},Y={}".format(x,y))
+            time.sleep(1)
+
+    def terminate_position(self):
+        self._running_position = False
+
+    def terminate_bot(self):
+        self._running_bot = False
+
+    def onClickedPosition(self):
+        cbutton = self.sender()
+        if cbutton.isChecked():
+            print("position on")
+            self._running_position = True
+            _thread.start_new_thread( self.get_position, ())
+        else:
+            print("position off")
+            self.terminate_position()
+
+    def onClickedBot(self):
+        cbutton = self.sender()
+        if cbutton.isChecked():
+            print("bot on")
+            self._running_bot = True
+            _thread.start_new_thread( self.rotine, ())
+        else:
+            print("bot off")
+            self.terminate_bot()
+            
+
+app = QApplication(sys.argv)
+screen = Window()
+screen.setWindowTitle("CCARS BOT")
+screen.show()
+sys.exit(app.exec_())
